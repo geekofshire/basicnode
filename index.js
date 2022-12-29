39a3b9fb19982098fb249f43f25fd3ec590f2971
Rohan <nickeyjha2@gmail.com>
2022-12-29 20:04:48 +0530

    added node file

---
+++ index.js
@@ -0,0 +1,40 @@
+const http=require('http');
+const fs=require('fs')
+
+
+http.createServer((req,res)=>{
+    if(req.url='/'){
+        fs.readFile('index.html',(err,data)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end
+        });
+    }
+    else if(req.url='/about'){
+        fs.readFile('about.html',(err,data)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end
+        });
+    }
+    else if(req.url='/contact.html'){
+        fs.readFile('contact.html',(err,data)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end
+        });
+    }
+    else{
+        fs.readFile('error_404.html',(err,data)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end
+        });
+    }
+}).listen(6969,()=>{
+    console.log('Port 6969')
+});
