f24f9cc8324a87f46e073e8b88a1bd078409781e
Rohan <nickeyjha2@gmail.com>
2022-12-31 16:05:34 +0530

    made code readable

--- index.js
+++ index.js
@@ -6,35 +6,33 @@ http.createServer((req,res)=>{
     if(req.url==='/'){
         fs.readFile('index.html',(err,data)=>{
             if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write(data)
-            return res.end
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
         });
     }
     else if(req.url==='/about'){
         fs.readFile('about.html',(err,data)=>{
             if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write(data)
-            return res.end
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
         });
     }
     else if(req.url==='/contact'){
         fs.readFile('contact.html',(err,data)=>{
             if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write(data)
-            return res.end
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
         });
     }
     else{
         fs.readFile('error_404.html',(err,data)=>{
             if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write(data)
-            return res.end
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
         });
     }
-}).listen(8080,()=>{
-    console.log('Port 8080')
-});
+}).listen(8080);
