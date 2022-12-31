3ca7f593e725c28e4c0210bcbce904973d2d4740
Rohan <nickeyjha2@gmail.com>
2022-12-31 17:06:14 +0530

    added links

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http=require('http');
 const fs=require('fs')
+const url=require('url');
 
-
+const page404=fs.readFileSync("error_404.html",(err,data)=>{
+    if(err) throw err;
+    return data;
+})
 http.createServer((req,res)=>{
     if(req.url==='/'){
         fs.readFile('index.html',(err,data)=>{
-            if(err) throw err;
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.write(page404);
+                return res.end();
+              }
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end();
@@ -13,7 +21,11 @@ http.createServer((req,res)=>{
     }
     else if(req.url==='/about'){
         fs.readFile('about.html',(err,data)=>{
-            if(err) throw err;
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.write(page404);
+                return res.end();
+              }
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end();
@@ -21,15 +33,11 @@ http.createServer((req,res)=>{
     }
     else if(req.url==='/contact'){
         fs.readFile('contact.html',(err,data)=>{
-            if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-    else{
-        fs.readFile('error_404.html',(err,data)=>{
-            if(err) throw err;
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.write(page404);
+                return res.end();
+              }
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end();
