1497bd05cbd52f119031a0e15f188b00ba0a0f27
Rohan <nickeyjha2@gmail.com>
2022-12-31 17:11:36 +0530

    fixed js

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
-const http=require('http');
-const fs=require('fs')
-const url=require('url');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-const page404=fs.readFileSync("error_404.html",(err,data)=>{
-    if(err) throw err;
-    return data;
-})
-http.createServer((req,res)=>{
-    if(req.url==='/'){
-        fs.readFile('index.html',(err,data)=>{
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                res.write(page404);
-                return res.end();
-              }
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) throw err;
+  return data;
+});
+
+http
+  .createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    let filename = "";
+    if (q.pathname === "/") {
+      filename = "." + "/index.html";
+    } else {
+      filename = "." + q.pathname;
     }
-    else if(req.url==='/about'){
-        fs.readFile('about.html',(err,data)=>{
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                res.write(page404);
-                return res.end();
-              }
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-    else if(req.url==='/contact'){
-        fs.readFile('contact.html',(err,data)=>{
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                res.write(page404);
-                return res.end();
-              }
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-}).listen(8080);
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write(page404);
+        return res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
+    });
+  })
+  .listen(8080);
