41c41a99d5d81c7a4883ef6bcd886ecc54964271
Rohan Jha <nickeyjha2@gmail.com>
2023-01-01 11:51:14 +0000

    Changed hyperlinks

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const fs=require('fs/promises');
 const port=8080;
 
 app.get('/',async (req,res)=>{
-    res.send(await fs.readFile('index.html','utf-8'))
+    res.send(await fs.readFile('./index.html','utf-8'));
 });
 
 
-app.get('/about', async (req,res)=>{
-    res.send(await fs.readFile('about.html','utf-8'))
+app.get('/about.html', async (req,res)=>{
+    res.send(await fs.readFile('./about.html','utf-8'));
 });
 
-app.get('/contact', async (req,res)=>{
-    res.send(await fs.readFile('contact.html','utf-8'))
+app.get('/contact.html', async (req,res)=>{
+    res.send(await fs.readFile('./contact.html','utf-8'));
 });
 
-app.use(async (res,req)=>{
-    res.status(404).send(await fs.readFile('/error_404.html','utf8'))
-});
+// app.use(async (res,req)=>{
+//     res.statusCode(404).send(await fs.readFile('/error_404.html','utf8'))
+// });
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("Listening on 8080......")
 });
